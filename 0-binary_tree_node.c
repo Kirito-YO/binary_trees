d102ca54460f527701fac5e240e65da41ac46b6f
Kirito-YO <ussf.lord@gmail>
2023-11-30 13:15:23 -0800

    update

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,24 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Create a binary node
- * @parent: node parent
- * @value: Value of the node
- * Return: the new node
+ * binary_tree_node - binary tree node
+ * @parent: Var 1 node parent
+ * @value: Var 2 value of the node
+ * Return: Null on failure or the new node
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
-
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
-	{
+	binary_tree_t *NV_node = malloc(sizeof(binary_tree_t));
+	if (NV_node == NULL)
 		return (NULL);
-	}
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	return (new_node);
+	NV_node->n = value;
+	NV_node->parent = parent;
+	NV_node->left = NULL;
+	NV_node->right = NULL;
+	return (NV_node);
 }
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,30 +1,25 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - add a node in the left of the parent
- * if it exists it move down one level and add the new node first
+ * binary_tree_insert_left - insert a node in the left as child.
  * @parent: parent of the specified node
  * @value: value of the node
  * Return: NULL if it fails or the new node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *NV_node;
 
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
 
-	new_node = binary_tree_node(parent, value);
-	if (new_node == NULL)
-	{
+	NV_node = binary_tree_node(parent, value);
+	if (NV_node == NULL)
 		return (NULL);
-	}
 	if (parent->left != NULL)
 	{
-		new_node->left = parent->left;
-		parent->left->parent = new_node;
+		NV_node->left = parent->left;
+		parent->left->parent = NV_node;
 	}
-	parent->left = new_node;
-	return (new_node);
+	parent->left = NV_node;
+	return (NV_node);
 }
